6f51a041da045da5b027a4b61c864e8447eb6f0a
+Sam Thongo <+samuelthongo73@gmail.com>
2023-06-28 13:55:13 +0300

    fetch lab

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(response => response.json())
+  .then(jsonData => {
+    renderBooks(jsonData)
+    return jsonData
+  })
+  .catch(error => console.log("Error Fetching Books", error))
 }
 
 function renderBooks(books) {
